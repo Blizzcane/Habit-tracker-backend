ff50f08712afd1ad7a55aa437167337794d92697
Blizzcane <blizzcane@gmail.com>
2022-01-01 22:15:41 -0600

    first commit

---
+++ src/db/migrations/20220102033628_habits.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("habits", (table) => {
+        table.increments("habit_id").primary();
+        table.string("habit_name");
+        table.boolean("habit_completed");
+        table.string("habit_days"); 
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable("habits");
+
+};
