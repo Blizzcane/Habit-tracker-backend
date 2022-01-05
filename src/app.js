66829bdffa8fe4eb0a5f8cdce96fcbb04f948bd1
Blizzcane <blizzcane@gmail.com>
2022-01-04 22:19:23 -0600

    added cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const habitsRouter = require("./habits/habits.router"); 
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/habits", habitsRouter); 
